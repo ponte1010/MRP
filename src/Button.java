1742be6791f42f8744d8752d032d9521863742f8
YukiShimazawa <54874117+YukiShimazawa@users.noreply.github.com>
2020-06-18 14:53:54 +0900

    Add files via upload

--- src/Button.java
+++ src/Button.java
@@ -12,6 +12,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Font;
 
 public class Button extends JFrame {
 
@@ -46,47 +47,61 @@ public class Button extends JFrame {
 	 */
 	public Button() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 535, 368);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		JButton btnNewButton = new JButton("Work Data");
+		btnNewButton.setBounds(126, 37, 109, 48);
 		btnNewButton.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
 		       constructBOM.printWorkdata();
 		    }
 		});
-		contentPane.add(btnNewButton, BorderLayout.EAST);
+		contentPane.setLayout(null);
+		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Parts in BOM");
+		btnNewButton_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 18));
+		btnNewButton_1.setBounds(345, 1, 163, 33);
 		btnNewButton_1.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
 		      constructBOM.printBOMStructure();
 		    }
 		});
-		contentPane.add(btnNewButton_1, BorderLayout.CENTER);
+		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Exit");
+		btnNewButton_2.setBounds(5, 40, 112, 42);
 		btnNewButton_2.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
 		    	System.out.println("Exit");
 		    }
 		});
-		contentPane.add(btnNewButton_2, BorderLayout.WEST);
+		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Order");
+		btnNewButton_3.setBounds(239, 37, 118, 48);
 		btnNewButton_3.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
 		    	constructBOM.printOrderData();
 		    }
 		});
-		contentPane.add(btnNewButton_3, BorderLayout.SOUTH);
+		contentPane.add(btnNewButton_3);
 		
 		textField = new JTextField();
-		contentPane.add(textField, BorderLayout.NORTH);
+		textField.setBounds(5, 5, 323, 25);
+		contentPane.add(textField);
 		textField.setColumns(10);
+		
+		JButton btnNewButton_4 = new JButton("MRP");
+		btnNewButton_4.setBounds(5, 105, 175, 48);
+		btnNewButton_4.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    	ReadWriteData.main(null);
+		    	}
+		});
+		contentPane.add(btnNewButton_4);
 	}
-
 }	
